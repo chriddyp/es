940fe04e7093606fd70c323eb73e7d14b589780d
chriddyp <chris@plot.ly>
2016-10-07 12:17:35 -0400

    build

--- scripts/App.js
+++ scripts/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import Select from 'react-select';
 
 const Table = props => {
@@ -25,6 +25,131 @@ const Table = props => {
     );
 }
 
+class RangeAggregation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ranges: [
+                {from: null, to: null}
+            ]
+        };
+
+        this.renderRange = this.renderRange.bind(this);
+    }
+
+    renderRange(index) {
+        const {onChange, type} = this.props;
+        // TODO - Add deleting ranges
+        const inputType = type === 'numeric' ? 'number' : 'text';
+        return (
+            <div>
+                <label>
+                    min
+                </label>
+                <input
+                    type={inputType}
+                    onChange={e => {
+                        const ranges = this.state.ranges;
+                        ranges[index].from = e.target.value;
+                        this.setState({ranges})
+                        onChange(ranges)
+                    }}
+                    value={this.state.ranges[index].from}
+                />
+
+                <label>
+                    max
+                </label>
+                <input
+                    type={inputType}
+                    onChange={e => {
+                        const ranges = this.state.ranges;
+                        ranges[index].to = e.target.value;
+                        this.setState({ranges})
+                        onChange(ranges)
+                    }}
+                    value={this.state.ranges[index].to}
+                />
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.ranges.map((v, i) => this.renderRange(i))}
+                <button onClick={() => {
+                    this.state.ranges.push({from: null, to: null});
+                    this.setState({ranges: this.state.ranges});
+                }}>
+                    Add Range
+                </button>
+            </div>
+        )
+    }
+};
+
+RangeAggregation.propTypes = {
+    onChange: PropTypes.func.isRequired,
+    type: PropTypes.oneOf(['numeric', 'date', 'ipv4'])
+};
+
+const FiltersAggregation = props => {
+    const {filters, setFilters, columns} = props;
+
+    return (
+        <div>
+            {filters.map(filter => {
+
+                return (
+                    <div style={{width: 300}}>
+                        <label>column</label>
+                        <Select
+                            options={columns.map(c=>({label: c, value: c}))}
+                            onChange={option=>{
+                                filters
+                            }}
+                        />
+
+                        <label>filter term</label>
+                        <input type="text"/>
+                    </div>
+                );
+
+            })}
+
+            <button onClick={() => {
+                filters.push({});
+                setFilters(filters)
+            }}>
+                Add filter
+            </button>
+
+        </div>
+    );
+}
+
+
+const AGGREGATION_PROPERTIES = {
+    terms: ['orderby', 'order', 'size'],
+    histogram: ['interval'],
+    range: ['ranges'],
+    date_range: ['ranges'],
+    ipv4_range: ['ranges'],
+    significant_terms: ['size'],
+    filters: []
+}
+
+const AGGREGATION_TYPES = {
+    terms: 'any',
+    histogram: 'numeric',
+    range: 'numeric',
+    date_range: 'date',
+    ipv4_range: 'ipv4',
+    significant_terms: 'string',
+    filters: 'any'
+}
+
 function outputJsonToColumnData(controls, outputJson) {
     let columns = [];
     let rows = [];
@@ -38,7 +163,10 @@ function outputJsonToColumnData(controls, outputJson) {
             columns = [];
             rows = [[]];
         } else {
-            columns = [controls.metricColumn, controls.metricType];
+            columns = [
+                `${controls.aggregationColumn}`,
+                `${controls.metricType} by ${controls.metricColumn}`
+            ];
             rows = buckets.map(bucket => [bucket.key, bucket.agg2.value]);
         }
     }
@@ -68,12 +196,15 @@ function controlsToJson(controls) {
         if (!controls.metricColumn) {
             inputJson = null;
         } else {
+            const propertyNames = AGGREGATION_PROPERTIES[controls.aggregationType];
+            const aggregation = propertyNames.reduce((r, v) => {
+                if(controls[v]) r[v] = controls[v];
+                return r;
+            }, {});
+            aggregation.field = controls.aggregationColumn;
             inputJson.aggs = {
                 'agg1': {
-                    'terms': {
-                        'field': controls.aggregationColumn
-                    },
-
+                    [controls.aggregationType]: aggregation,
                     'aggs': {
                         'agg2': {
                             [controls.metricType]: {
@@ -94,7 +225,24 @@ export default class App extends Component {
         this.state = {
             controls: {
                 search: '*',
+
+                aggregationType: '',
                 aggregationColumn: '',
+
+                // aggregation properties
+                // term aggregation
+                orderby: null,
+                order: null,
+                size: null,
+
+                // histogram aggregation
+                interval: null,
+
+                // range* aggregations
+                ranges: null,
+
+                // filters aggregation
+                filters: [],
 
                 metricType: '',
                 metricColumn: ''
@@ -117,15 +265,47 @@ export default class App extends Component {
         const {controls, columnTypes} = this.state;
         controls[controlId] = value;
 
-        // Some controls depend on each other
+        // set metricColumn and metricType to count when they are undefined
         if (controls.aggregationColumn &&
-            (controls.metricType === 'value_count' ||
-             controls.metricType === '')
+            controls.metricType === ''
         ) {
-            // elasticsearch is implicitly computing 'count' over the aggregationColumn
+
             controls.metricColumn = controls.aggregationColumn;
             controls.metricType = 'value_count';
         }
+
+        /*
+         * some metric types are type specific.
+         * clear the column if the type isn't valid
+         */
+        if (controls.aggregationColumn) {
+            if (controls.aggregationType === 'terms') {
+                // any type is OK
+            } else if (controls.aggregationType === 'histogram') {
+                if (columnTypes[controls.aggregationColumn] !== 'numeric') {
+                    controls.aggregationColumn = '';
+                }
+            } else if (controls.aggregationType === 'range') {
+                if (columnTypes[controls.aggregationColumn] !== 'numeric') {
+                    controls.aggregationColumn = '';
+                }
+            } else if (controls.aggregationType === 'date_range') {
+                if (columnTypes[controls.aggregationColumn] !== 'date') {
+                    controls.aggregationColumn = '';
+                }
+            } else if (controls.aggregationType === 'ipv4_range') {
+                if (columnTypes[controls.aggregationColumn] !== 'ipv4') {
+                    controls.aggregationColumn = '';
+                }
+            } else if (controls.aggregationType === 'range') {
+                if (columnTypes[controls.aggregationColumn] !== 'numeric') {
+                    controls.aggregationColumn = '';
+                }
+            } else if (controls.aggregationType === 'significant_terms') {
+                // i think any type is OK here
+            }
+        }
+
 
         /*
          * Every metric besides value_counts requires a numeric column.
@@ -135,9 +315,10 @@ export default class App extends Component {
         if (controls.metricType !== 'value_count' &&
             columnTypes[controls.metricColumn] !== 'numeric'
         ) {
-            controls.metricColumn = null;
+            controls.metricColumn = '';
         }
 
+        controls[controlId] = value; // TODO - fix up this json serialization and reserialization
         this.setState(this.state);
         this.executeQuery(controls)
 
@@ -145,12 +326,19 @@ export default class App extends Component {
 
     rawQuery(payload) {
         const protocol = window.location.protocol;
+        // TODO - get elastic.co to work with https
         console.warn('protocol: ', protocol);
-        const elasticUrl = '//67a7441549120daa2dbeef8ac4f5bb2e.us-east-1.aws.found.io:9200';
+        let elasticUrl;
+        if (protocol === 'https:') {
+            elasticUrl = 'https://67a7441549120daa2dbeef8ac4f5bb2e.us-east-1.aws.found.io:9243';
+        } else {
+            elasticUrl = 'http://67a7441549120daa2dbeef8ac4f5bb2e.us-east-1.aws.found.io:9200';
+        }
+
         const indexName = 'sample-data';
         const typeId = 'test-type';
         this.setState({status: 'loading...'});
-        const request = `${protocol}${elasticUrl}/${indexName}/_search`;
+        const request = `${elasticUrl}/${indexName}/_search`;
         console.warn('request: ', request);
         const body = JSON.stringify(payload);
 
@@ -241,18 +429,161 @@ export default class App extends Component {
             </div>
         );
 
-        const groupby = (
+        const aggregationMetric = (
             <div>
                 <label>
-                    Group by
+                    aggregation by
                 </label>
                 <div style={{width: 300}}>
                     <Select
-                        options={elasticsearchColumns.map(c => ({label: c, value: c}))}
-                        onChange={option => this.updateControl('aggregationColumn', option.value)}
-                        value={controls.aggregationColumn}
+                        options={[
+                            {label: 'terms', value: 'terms'},
+                            {label: 'histogram', value: 'histogram'},
+                            {label: 'range', value: 'range'},
+                            {label: 'date_range', value: 'date_range'},
+                            {label: 'ipv4_range', value: 'ipv4_range'},
+                            {label: 'filters', value: 'filters'},
+                            {label: 'significant_terms', value: 'significant_terms'}
+                        ]}
+                        onChange={option => this.updateControl('aggregationType', option ? option.value : '')}
+                        value={controls.aggregationType}
                     />
                 </div>
+            </div>
+        );
+
+        let aggregationColumn = null;
+        if (controls.aggregationType !== 'filters') {
+            aggregationColumn = (
+                <div>
+                    <label>
+                        over column
+                    </label>
+                    <div style={{width: 300}}>
+                        <Select
+                            options={elasticsearchColumns.map(c => (
+                                {
+                                    label: c,
+                                    value: c,
+                                    disabled: !(
+                                        AGGREGATION_TYPES[controls.aggregationType] === 'any' ||
+                                        columnTypes[c] === AGGREGATION_TYPES[controls.aggregationType]
+                                    )
+                                }
+                            ))}
+                            onChange={option => this.updateControl('aggregationColumn', option ? option.value : '')}
+                            value={controls.aggregationColumn}
+                            disabled={!controls.aggregationType}
+                        />
+                    </div>
+                </div>
+            );
+        }
+
+        let aggregationProperties = null;
+        if (controls.aggregationType === 'terms') {
+            // orderby (metric like count)
+            // order - ascending, descending
+            // size - integer
+            aggregationProperties = (
+                <div>
+                    <div style={{width: 300}}>
+                        <label>
+                            order
+                        </label>
+                        <Select
+                            options={[
+                                {label: 'ascending alphabetical', value: JSON.stringify({'_term': 'asc'})},
+                                {label: 'descending alphabetical', value: JSON.stringify({'_term': 'desc'})},
+                                {label: 'ascending count', value: JSON.stringify({'_count': 'asc'})},
+                                {label: 'descending count', value: JSON.stringify({'_count': 'desc'})}
+                            ]}
+                            value={JSON.stringify(controls.order)}
+                            onChange={option => this.updateControl('order', JSON.parse(option.value))}
+                        />
+                    </div>
+
+                    <div>
+                        <label>
+                            size
+                        </label>
+                        <input
+                            type="number"
+                            onChange={e => this.updateControl('size', e.target.value)}
+                            value={controls.size}
+                        />
+                    </div>
+                </div>
+            );
+            // TODO - Add orderby aggregation
+        } else if (controls.aggregationType === 'histogram') {
+            aggregationProperties = (
+                <div>
+                    <label>
+                        with interval
+                    </label>
+                    <input
+                        type="number"
+                        onChange={e => this.updateControl('interval', e.target.value)}
+                        value={controls.interval}
+                    />
+                </div>
+            );
+        } else if (controls.aggregationType === 'range') {
+            aggregationProperties = (
+                // TODO - ranges state should be managed entirely by this component
+                // so that we can pass initial values
+                <RangeAggregation
+                    type={'numeric'}
+                    onChange={ranges => this.updateControl('ranges', ranges)}
+                />
+            );
+        } else if (controls.aggregationType === 'date_range') {
+            aggregationProperties = (
+                <RangeAggregation
+                    type={'date'}
+                    onChange={ranges => this.updateControl('ranges', ranges)}
+                />
+            );
+        } else if (controls.aggregationType === 'ipv4_range') {
+            aggregationProperties = (
+                <RangeAggregation
+                    type={'ipv4'}
+                    onChange={ranges => this.updateControl('ranges', ranges)}
+                />
+            );
+        } else if (controls.aggregationType === 'filters') {
+            aggregationProperties = (
+                <FiltersAggregation
+                    columns={elasticsearchColumns}
+                    filters={controls.filters}
+                    setFilters={filters => {
+                        this.state.controls.filters = filters;
+                        this.setState({controls: this.state.controls});
+                    }}
+                />
+            );
+        } else if (controls.aggregationType === 'significant_terms') {
+            aggregationProperties = (
+                <div>
+                    <label>
+                        size
+                    </label>
+                    <input
+                        type="number"
+                        onChange={e => this.updateControl('size', e.target.value)}
+                        value={controls.size}
+                    />
+                </div>
+            );
+        }
+
+
+        const groupby = (
+            <div>
+                {aggregationMetric}
+                {aggregationColumn}
+                {aggregationProperties}
             </div>
         );
 
@@ -272,13 +603,13 @@ export default class App extends Component {
                             {label: 'cardinality', value: 'cardinality'}
                         ]}
                         value={controls.metricType}
-                        onChange={option => this.updateControl('metricType', option.value)}
+                        onChange={option => this.updateControl('metricType', option ? option.value : '')}
                         disabled={controls.metricColumn === ''}
                     />
                 </div>
 
                 <label>
-                    Over column
+                    over column
                 </label>
                 <div style={{width: 300}}>
                     <Select
@@ -290,11 +621,15 @@ export default class App extends Component {
                                 controls.metricType !== 'value_count'
                             )
                         }))}
-                        onChange={option => this.updateControl('metricColumn', option.value)}
+                        onChange={option => this.updateControl('metricColumn', option ? option.value : '')}
                         value={controls.metricColumn}
-                        disabled={!controls.metricType || controls.metricType === 'value_count'}
+                        disabled={
+                            !Boolean(controls.metricType) ||
+                            (controls.metricType === 'value_count')
+                        }
                     />
                 </div>
+
             </div>
         )
 
@@ -313,14 +648,6 @@ export default class App extends Component {
                     <div className="six columns">
                         {computeby}
                     </div>
-                </div>
-
-                <div>
-                    {this.state.status}
-                </div>
-
-                <div style={{color: 'red'}}>
-                    {this.state.outputError}
                 </div>
 
                 <hr/>
